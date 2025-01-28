e169d305dc1a10605c3d372643cde82eed2e7985
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-01-28 17:25:44 +0300

    Update generics.ts

--- src/Utils/generics.ts
+++ src/Utils/generics.ts
@@ -199,7 +199,7 @@ export const generateMessageIDV2 = (userId?: string): string => {
 	random.copy(data, 28)
 
 	const hash = createHash('sha256').update(data).digest()
-	return '3EB0' + hash.toString('hex').toUpperCase().substring(0, 18)
+	return 'DREADED' + hash.toString('hex').toUpperCase().substring(0, 18)
 }
 
 // generate a random ID to attach to a message
